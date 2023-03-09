a59e2290e53cab803f0dc88849c0a83a8061eb48
Dropqt <nikola.s.blagojevic@gmail.com>
2023-03-09 20:54:00 +0100

    Check, checkmate and stalemate with naive algorithm

--- ChessEngine.py
+++ ChessEngine.py
@@ -9,7 +9,7 @@ class GameState():
             ['bR','bN','bB','bQ','bK','bB','bN','bR'],
             ['bp','bp','bp','bp','bp','bp','bp','bp'],
             ['--','--','--','--','--','--','--','--'],
-            ['--','--','--','--','bK','--','--','--'],
+            ['--','--','--','--','--','--','--','--'],
             ['--','--','--','--','--','--','--','--'],
             ['--','--','--','--','--','--','--','--'],
             ['wp','wp','wp','wp','wp','wp','wp','wp'],
@@ -18,12 +18,22 @@ class GameState():
         self.moveLog=[]
         self.moveFunctions={'p':self.getPawnMoves,'R':self.getRookMoves,'N':self.getKnightMoves,
                             'B':self.getBishopMoves,'Q':self.getQueenMoves,'K':self.getKingMoves}
+        self.whiteKingLocation=(7,4)
+        self.blackKingLocation=(0,4)
+        self.checkMate= False
+        self.staleMate= False
+        
         
     def makeMove(self,move):
         self.board[move.startRow][move.startCol]= '--'
         self.board[move.endRow][move.endCol]= move.pieceMoved
         self.moveLog.append(move)# log the move for undo purposes or history
         self.whiteToMove= not self.whiteToMove # swap players
+        #update kings location if moved
+        if move.pieceMoved=='wK':
+            self.whiteKingLocation=(move.endRow,move.endCol)
+        elif move.pieceMoved=='bK':
+            self.blackKingLocation=(move.endRow,move.endCol)
         """
         Undo the last move made
         """
@@ -33,13 +43,58 @@ class GameState():
             self.board[move.startRow][move.startCol]=move.pieceMoved
             self.board[move.endRow][move.endCol]=move.pieceCaptured
             self.whiteToMove= not self.whiteToMove
+            #update the king's position if needed
+            if move.pieceMoved=='wK':
+                self.whiteKingLocation=(move.startRow, move.startCol)
+            elif move.pieceMoved=='bK':
+                self.blackKingLocation=(move.startRow, move.startCol)
     
     
     
     #all moves considering checks
     def getValidMoves(self):
-        return self.getAllPossibleMoves()
-    
+        #1. Generate all possible moves
+        moves=self.getAllPossibleMoves()
+        #2. For each move, make the move
+        for i in range(len(moves)-1,-1,-1): #when removing from a list, go backwards
+            self.makeMove(moves[i])
+            #3. Generate all opponents moves
+            #4. For each of your opponents moves, see if they attack your king
+            self.whiteToMove= not self.whiteToMove
+            if self.inCheck():
+                moves.remove(moves[i]) #5. if they do attack your king, its not a valid move
+            self.whiteToMove= not self.whiteToMove
+            self.undoMove()
+        if len(moves)== 0: #either checkmate or stalemate
+            if self.inCheck():
+                self.checkMate= True
+            else:
+                self.staleMate= True
+        else:
+            self.checkMate= False
+            self.staleMate= False
+        return moves
+    """Check if current player is in check
+    """
+    def inCheck(self):
+        if self.whiteToMove:
+            return self.squareUnderAttack(self.whiteKingLocation[0],self.whiteKingLocation[1])
+        else:
+            return self.squareUnderAttack(self.blackKingLocation[0],self.blackKingLocation[1])
+        
+    """Check if the enemy can attack the square r,c
+    """
+    def squareUnderAttack(self,r,c):
+        self.whiteToMove= not self.whiteToMove #switch to opponents turn
+        oppMoves=self.getAllPossibleMoves()
+        self.whiteToMove= not self.whiteToMove
+        for move in oppMoves:
+            if move.endRow== r and move.endCol== c: #square is under attack
+                return True
+        return False
+        
+        
+        
     #all moves without considering checks
     def getAllPossibleMoves(self):
         moves=[]
